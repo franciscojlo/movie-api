e67735d689c77175d221424595acb2a5ba895bed
franciscojlo <120750470+franciscojlo@users.noreply.github.com>
2023-07-19 18:11:06 -0700

    updated server.js
    updated code to return index.html or documentation.html in replace of string response using the readFile method

--- server.js
+++ server.js
@@ -8,15 +8,8 @@ http.createServer((request, response) => {
 
   const containsDocumentation = pathname.includes('documentation');
 
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-
-  if (containsDocumentation) {
-    response.end('This URL contains the word "documentation".\n');
-  } else {
-    response.end(`Hello Node!.\n`);
-  }
-
   const logData = `URL: ${request.url}\nTimestamp: ${new Date().toLocaleString()}\n\n`;
+
   fs.appendFile('log.txt', logData, (err) => {
     if (err) {
       console.error('Error writing to log.txt:', err);
@@ -24,6 +17,32 @@ http.createServer((request, response) => {
       console.log('Request logged successfully.');
     }
   });
+
+  if (containsDocumentation) {
+    fs.readFile('documentation.html', (err, data) => {
+      if (err) {
+        console.error('Error reading documentation.html:', err);
+        response.writeHead(500, { 'Content-Type': 'text/plain' });
+        response.end('Internal Server Error');
+      } else {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.write(data);
+        response.end();
+      }
+    });
+  } else {
+    fs.readFile('index.html', (err, data) => {
+      if (err) {
+        console.error('Error reading index.html:', err);
+        response.writeHead(500, { 'Content-Type': 'text/plain' });
+        response.end('Internal Server Error');
+      } else {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.write(data);
+        response.end();
+      }
+    });
+  }
 }).listen(8080);
 
 console.log('My first Node test server is running on Port 8080.');
