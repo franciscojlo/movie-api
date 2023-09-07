08e28acda34c3c9ff35b7b946b7e5a34a9d2d275
franciscojlo <120750470+franciscojlo@users.noreply.github.com>
2023-09-06 18:10:42 -0700

    mongodb connection issue

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+const Movies = Models.Movie;
+const Users = Models.User;
+
+mongoose.connect('mongodb://localhost:27017/myflix', { useNewUrlParser: true, useUnifiedTopology: true });
 const express = require('express');
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const uuid = require('uuid');
@@ -81,109 +90,158 @@ app.get('/', (req, res) => {
 });
 
 //route to get all movies
-app.get('/movies', (req, res) => {
-  res.send('Successful GET request returning data on all the movies');
+app.get('/movies', async (req, res) => {
+  try {
+    const movies = await Movies.find();
+    if (!movies) {
+      return res.status(404).send('No movies found');
+    }
+    res.json(movies);
+  } catch (error) {
+    console.error('Error fetching movies:', error);
+    res.status(500).send('An error occurred while fetching movies.');
+  }
 });
 
 //route to get a single movie by title
-app.get('/movies/:title', (req, res) => {
+app.get('/movies/:title', async (req, res) => {
   const movieTitle = req.params.title;
-  res.send(`Successful GET request returning data on the movie with title: ${movieTitle}`);
+  try {
+    const movie = await Movies.findOne({ Title: movieTitle });
+    if (!movie) {
+      return res.status(404).send('Movie not found');
+    }
+    res.json(movie);
+  } catch (error) {
+    console.error('Error fetching movie by title:', error);
+    res.status(500).send('An error occurred while fetching the movie.');
+  }
 });
 
 //route to get genre data by name
-app.get('/genres/:name', (req, res) => {
+app.get('/genres/:name', async (req, res) => {
   const genreName = req.params.name;
-  res.send(`Successful GET request returning data on the genre with name: ${genreName}`);
+  try {
+    const genre = await Movies.distinct('Genre.Name', { 'Genre.Name': genreName });
+    if (!genre.length) {
+      return res.status(404).send('Genre not found');
+    }
+    res.json({ Name: genre[0] });
+  } catch (error) {
+    console.error('Error fetching genre by name:', error);
+    res.status(500).send('An error occurred while fetching the genre.');
+  }
 });
 
 //route to get director data by name
-app.get('/directors/:name', (req, res) => {
+app.get('/directors/:name', async (req, res) => {
   const directorName = req.params.name;
-  res.send(`Successful GET request returning data on the director with name: ${directorName}`);
+  try {
+    const director = await Movies.findOne({ 'Director.Name': directorName }, 'Director.Name Director.Bio Director.Birth');
+    if (!director) {
+      return res.status(404).send('Director not found');
+    }
+    res.json(director.Director);
+  } catch (error) {
+    console.error('Error fetching director by name:', error);
+    res.status(500).send('An error occurred while fetching the director.');
+  }
 });
 
 //route to allow new users to register
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
   const { name, password, email, birth_date } = req.body;
-  if (!name || !password || !email) {
-    return res.status(400).send('Name, password, and email are required fields');
+  try {
+    const newUser = await Users.create({
+      Username: name,
+      Password: password,
+      Email: email,
+      Birthday: birth_date,
+      FavoriteMovies: []
+    });
+    res.status(201).json(newUser);
+  } catch (error) {
+    console.error('Error creating a new user:', error);
+    res.status(400).send('User registration failed.');
   }
-  const newUser = {
-    id: uuid.v4(),
-    name: name,
-    password: password,
-    email: email,
-    birth_date: birth_date,
-    favoriteMovies: [],
-  };
-  users.push(newUser);
-  res.status(201).json(newUser);
 });
 
 //route to allow users to update their username
-app.put('/users/:id', (req, res) => {
-  const userId = req.params.id;
-  const { name } = req.body;
-  
-  console.log('User ID from URL:', userId);
-  
-  const user = users.find((user) => user.id === userId);
-  console.log('Found User:', user);
-  
-  if (!user) {
-    return res.status(404).send('User not found');
+app.put('/users/:userId', async (req, res) => {
+  const userId = req.params.userId;
+  const updatedUserData = req.body;
+  try {
+    const updatedUser = await Users.findByIdAndUpdate(userId, updatedUserData, { new: true });
+    if (!updatedUser) {
+      return res.status(404).send('User not found');
+    }
+    res.json(updatedUser);
+  } catch (error) {
+    console.error('Error updating user info:', error);
+    res.status(400).send('User update failed.');
   }
-  
-  user.name = name;
-  console.log('Updated User:', user);
-  
-  res.json(user);
 });
 
 //route to allow users to add a movie to their favorites
-app.post('/users/:id/favorites', (req, res) => {
-  const userId = req.params.id;
+app.post('/users/:userId/favorites', async (req, res) => {
+  const userId = req.params.userId;
   const { movieId } = req.body;
-  const user = users.find((user) => user.id === userId);
-  if (!user) {
-    return res.status(404).send('User not found');
+  try {
+    const user = await Users.findById(userId);
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+    user.FavoriteMovies.push(movieId);
+    await user.save();
+    res.send('Movie added to user favorites successfully');
+  } catch (error) {
+    console.error('Error adding a movie to user favorites:', error);
+    res.status(500).send('An error occurred while adding the movie to user favorites.');
   }
-  user.favoriteMovies.push(movieId);
-  res.send('Movie added to user favorites successfully');
 });
 
 //route to allow users to remove a movie from their list of favorites
-app.delete('/users/:id/favorites/:movieId', (req, res) => {
-  const userId = req.params.id;
+app.delete('/users/:userId/favorites/:movieId', async (req, res) => {
+  const userId = req.params.userId;
   const movieId = req.params.movieId;
-  const user = users.find((user) => user.id === userId);
-  if (!user) {
-    return res.status(404).send('User not found');
+  try {
+    const user = await Users.findById(userId);
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+    const index = user.FavoriteMovies.indexOf(movieId);
+    if (index !== -1) {
+      user.FavoriteMovies.splice(index, 1);
+      await user.save();
+      res.send('Movie removed from user favorites successfully');
+    } else {
+      res.status(404).send('Movie not found in user favorites');
+    }
+  } catch (error) {
+    console.error('Error removing a movie from user favorites:', error);
+    res.status(500).send('An error occurred while removing the movie from user favorites.');
   }
-  const index = user.favoriteMovies.indexOf(movieId);
-  if (index !== -1) {
-    user.favoriteMovies.splice(index, 1);
-  }
-  res.send('Movie removed from user favorites successfully');
 });
 
-//route to allow existing users to deregister
-app.delete('/users/:id', (req, res) => {
-  const userId = req.params.id;
-  const index = users.findIndex((user) => user.id === userId);
-  if (index !== -1) {
-    users.splice(index, 1);
+// Route to allow existing users to deregister
+app.delete('/users/:userId', async (req, res) => {
+  const userId = req.params.userId;
+  try {
+    const user = await Users.findByIdAndRemove(userId);
+    if (!user) {
+      return res.status(404).send(`User with ID ${userId} not found`);
+    }
     res.send(`User with ID ${userId} unregistered successfully`);
-  } else {
-    res.status(404).send('User not found');
+  } catch (error) {
+    console.error('Error deregistering user:', error);
+    res.status(500).send('An error occurred while deregistering the user.');
   }
 });
 
 //error-handling middleware
 app.use((err, req, res, next) => {
   console.error('APPLICATION ERROR:', err.stack);
-  res.status(500).send("Something went wrong.");
+  res.status(500).send('Something went wrong.');
 });
 
 const port = 8080;
